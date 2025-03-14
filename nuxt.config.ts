c612b15c4561f47d8323d86a9d4f63d53fe884d8
mehmetsel <mehmet.sel@useinsider.com>
2025-03-15 01:57:55 +0300

    move to root file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,17 @@
 export default defineNuxtConfig({
   css: ['~/assets/tailwind.css', '~/assets/base.css'],
-  
   modules: [
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
   ],
-
   build: {
     transpile: ['axios']
   },
-
   vite: {
     optimizeDeps: {
       include: ['form-data']
     }
   },
-
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   pages: true,
